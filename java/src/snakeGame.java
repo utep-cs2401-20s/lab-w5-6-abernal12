1ad1186b52bdd648a1ac5339522320b45454419a
abernal12 <53953393+abernal12@users.noreply.github.com>
2020-02-17 14:26:02 -0700

    created default constructor.

---
+++ java/src/snakeGame.java
@@ -0,0 +1,15 @@
+public class snakeGame {
+
+    private boolean[][] game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    // default constructor //
+
+    public snakeGame(){
+        int[][] gameBoard = new 
+    }
+
+
+}
