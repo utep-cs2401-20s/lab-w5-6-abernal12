04360d7c74fdbf78065dd574cfb36638516002f4
abernal12 <53953393+abernal12@users.noreply.github.com>
2020-02-29 21:43:44 -0700

    created the remaining test cases for the exhaustive.

--- java/src/snakeGameTester.java
+++ java/src/snakeGameTester.java
@@ -10,146 +10,94 @@
 
 class snakeGameTester {
 
-        /*
-         * findTailExhaustive:
-         * Test case: 1
-         * this test case is supposed to locate the tail and get the exact x and y coordinates, along with the length of the tail.
-         * However, the test is failing.
-         *
-         * this test case should be returning {3, 3, 7}
-         */
+    /*
+     * findTailExhaustive:
+     * Test case: 1
+     * this test case is supposed to locate the tail and get the exact x and y coordinates, along with the length of the tail.
+     * However, the test is failing.
+     *
+     * this test case should be returning {3, 3, 7}
+     */
 
-        @Test
-        public void testfindTailExhaustive1() { //NUMBER 1//
-            boolean[][] board = {{true, true, false, false},
-                                {false, true, false, false},
-                                {false, true, true, false},
-                                {false, false, true, true}};
+    @Test
+    public void testfindTailExhaustive1() { //NUMBER 1//
+        boolean[][] board = {{true, true, false, false},
+                {false, true, false, false},
+                {false, true, true, false},
+                {false, false, true, true}};
 
-            snakeGame game = new snakeGame(board, 0, 0);
-            int[] result = game.findTailExhaustive();
-            assertArrayEquals(new int[]{3, 3, 7}, result);
-        }
+        snakeGame game = new snakeGame(board, 0, 0);
+        int[] result = game.findTailExhaustive();
+        assertArrayEquals(new int[]{3, 3, 7}, result);
+    }
 
-        /*
-         * findTailExhaustive:
-         * Test case: 1
-         * this test case is supposed to locate the tail and get the exact x and y coordinates, along with the length of the tail.
-         * However, the test is failing.
-         *
-         * this test case should return {3, 3, 16}
-         */
+    /*
+     * findTailExhaustive:
+     * Test case: 2
+     * this test case is supposed to locate the tail and get the exact x and y coordinates, along with the length of the tail.
+     * However, the test is failing.
+     *
+     * this test case should return {3, 3, 16}
+     */
 
-        @Test
-        public void testfindTailExhaustive2() { //NUMBER 2//
-            boolean[][] board = {{true, true, true, true},
-                                {true, true, true, true},
-                                {true, true, true, true},
-                                {true, true, true, true}};
+    @Test
+    public void testfindTailExhaustive2() { //NUMBER 2//
+        boolean[][] board = {{true, true, true, true},
+                {true, true, true, true},
+                {true, true, true, true},
+                {true, true, true, true}};
 
-            snakeGame game = new snakeGame(board, 0, 0);
-            int[] result = game.findTailExhaustive();
-            assertArrayEquals(new int[]{3, 3, 16}, result);
-        }
-        /*
-         *
-         */
+        snakeGame game = new snakeGame(board, 0, 0);
+        int[] result = game.findTailExhaustive();
+        assertArrayEquals(new int[]{3, 3, 16}, result);
+    }
 
-//        @Test
-//        public void testfindTailExhaustive2() { //NUMBER 2//
-//            snakeGame sol = new snakeGame();
-//            int[] result = {4, 0, 5};
-//            assertEquals(result, sol.findTailExhaustive());
-//        }
+    /*
+     *tests whether if the method reads just one part of the game board.
+     * supposed to return {3, 3, 1}
+     */
+    @Test
+    public void testfindTailExhaustive3() { //NUMBER 2//
+        boolean[][] board = {{false, false, false, false},
+                            {false, false, false, false},
+                            {false, false, false, false},
+                            {false, false, false, true}};
 
-        /*
-         *
-         *
-         */
+        snakeGame game = new snakeGame(board, 0, 0);
+        int[] result = game.findTailExhaustive();
+        assertArrayEquals(new int[]{3, 3, 1}, result);
+    }
 
-//        @Test
-//        public void testfindTailExhaustive3() { //NUMBER 3//
-//            snakeGame sol = new snakeGame();
-//            int[] A = {1,2,3};
-//            assertEquals(A, sol.findTailExhaustive());
-//        }
+    /*
+     * test will see if the method will be able to function if there are no parts of the snake within the game board.
+     * should return an out of bounds exception or an error.
+     */
+    @Test
+    public void testfindTailExhaustive4() { //NUMBER 2//
+        boolean[][] board = {{false, false, false, false},
+                            {false, false, false, false},
+                            {false, false, false, false},
+                            {false, false, false, false}};
 
-        /*
-         *
-         */
+        snakeGame game = new snakeGame(board, 0, 0);
+        int[] result = game.findTailExhaustive();
+        assertArrayEquals(new int[]{0, 0, 0}, result);
+    }
 
-        @Test
-        public void testfindTailExhaustive4() { //NUMBER 4//
-            snakeGame sol = new snakeGame();
-            int[] A = {1,2,3};
-            assertEquals(3, sol.findTailExhaustive());
-        }
+    /*
+    edge case:
+     *check to see if the method will still add to the length and count the parts of the true portions of the game borad even though they are not together.
+     *should return an error
+     */
+    @Test
+    public void testfindTailExhaustive5() { //NUMBER 2//
+        boolean[][] board = {{true, false, false, false},
+                            {false, false, true, false},
+                            {false, false, false, false},
+                            {false, false, false, true}};
 
-        /*
-         *
-         */
-
-        @Test
-        public void testfindTailExhaustive5() { //NUMBER 5//
-            snakeGame sol = new snakeGame();
-            int[] A = {1,2,3};
-            assertEquals(3, sol.findTailExhaustive());
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////
-
-        /*
-         *
-         */
-
-        @Test
-        public void testfindTailRecursive1() { //NUMBER 1//
-            snakeGame sol = new snakeGame();
-            int[] A = {1,2,3};
-            assertEquals(3, sol.findTailRecursive());
-        }
-
-        /*
-         *
-         */
-
-        @Test
-        public void testfindTailRecursive2() { //NUMBER 2//
-            snakeGame sol = new snakeGame();
-            int[] A = {1,2,3};
-            assertEquals(3, sol.findTailRecursive());
-        }
-
-        /*
-         *
-         */
-
-        @Test
-        public void testfindTailRecursive3() { //NUMBER 3//
-            snakeGame sol = new snakeGame();
-            int[] A = {1,2,3};
-            assertEquals(3, sol.findTailRecursive());
-        }
-
-        /*
-         *
-         */
-
-        @Test
-        public void testfindTailRecursive4() { //NUMBER 4//
-            snakeGame sol = new snakeGame();
-            int[] A = {1,2,3};
-            assertEquals(3, sol.findTailRecursive());
-        }
-
-        /*
-         *
-         */
-
-        @Test
-        public void testfindTailRecursive5() { //NUMBER 5//
-            snakeGame sol = new snakeGame();
-            int[] A = {1,2,3};
-            assertEquals(3, sol.findTailRecursive());
-        }
+        snakeGame game = new snakeGame(board, 0, 0);
+        int[] result = game.findTailExhaustive();
+        assertArrayEquals(new int[]{0, 0, 0}, result);
+    }
 }
